0192374e1ee04f5bb7fba8d4e8b88348751f2238
THANH_PC\HaVan <thanhahvan1@gmail.com>
2017-12-06 22:45:45 +0700

    Update testing function.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,42 @@
-const functions = require('firebase-functions');
+let functions = require('firebase-functions');
+let admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.sendNotification = functions.database.ref('/notifications/messages/{pushId}')
+    .onWrite(event => {
+        const message = event.data.current.val();
+        const senderUid = message.from;
+        const receiverUid = message.to;
+        const promises = [];
+
+        if (senderUid == receiverUid) {
+            //if sender is receiver, don't send notification
+            promises.push(event.data.current.ref.remove());
+            return Promise.all(promises);
+        }
+
+        const getInstanceIdPromise = admin.database().ref(`/users/${receiverUid}/instanceId`).once('value');
+        const getSenderUidPromise = admin.auth().getUser(senderUid);
+
+        return Promise.all([getInstanceIdPromise, getSenderUidPromise]).then(results => {
+            const instanceId = results[0].val();
+            const sender = results[1];
+            console.log('notifying ' + receiverUid + ' about ' + message.body + ' from ' + senderUid);
+
+            const payload = {
+                notification: {
+                    title: sender.displayName,
+                    body: message.body,
+                    icon: sender.photoURL
+                }
+            };
+
+            admin.messaging().sendToDevice(instanceId, payload)
+                .then(function (response) {
+                    console.log("Successfully sent message:", response);
+                })
+                .catch(function (error) {
+                    console.log("Error sending message:", error);
+                });
+        });
+    });
