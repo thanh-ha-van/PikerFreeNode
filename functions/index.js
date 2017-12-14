78cdc5713686d1229fadac66039b3e84e03c8a41
THANH_PC\HaVan <thanhahvan1@gmail.com>
2017-12-15 00:22:42 +0700

    add functions trigger of new follower.

--- functions/index.js
+++ functions/index.js
@@ -27,7 +27,6 @@ exports.sendNotification = functions.database.ref('/notifications/messages/{push
                 notification: {
                     title: sender.displayName,
                     body: message.body,
-                    icon: sender.photoURL
                 }
             };
 
@@ -38,5 +37,62 @@ exports.sendNotification = functions.database.ref('/notifications/messages/{push
                 .catch(function (error) {
                     console.log("Error sending message:", error);
                 });
+
+            // delete node after send the mess;
+            var adaRef = admin.database().ref('/notifications/messages/{pushId}');
+            adaRef.remove()
+                .then(function () {
+                    console.log("Remove succeeded.")
+                })
+                .catch(function (error) {
+                    console.log("Remove failed: " + error.message)
+                });
         });
     });
+
+/**
+ * Triggers when a user gets a new follower
+ */
+exports.sendFollowerNotification = functions.database.ref('/notifications/followers/{pushId}')
+    .onWrite(event => {
+        const message = event.data.current.val();
+        const senderUid = message.from;
+        const receiverUid = message.to;
+        const promises = [];
+
+        const getInstanceIdPromise = admin.database().ref(`/users/${receiverUid}/instanceId`).once('value');
+        const getSenderUidPromise = admin.auth().getUser(senderUid);
+
+        return Promise.all([getInstanceIdPromise, getSenderUidPromise]).then(results => {
+
+            const instanceId = results[0].val();
+            const sender = results[1];
+            console.log('notifying ' + receiverUid + ' about ' + message.body + ' from ' + senderUid);
+
+            const payload = {
+                notification: {
+                    title: "You have new follower!",
+                    body: sender.displayName + "is following you",
+                }
+            };
+
+            admin.messaging().sendToDevice(instanceId, payload)
+                .then(function (response) {
+                    console.log("Successfully sent message:", response);
+                })
+                .catch(function (error) {
+                    console.log("Error sending message:", error);
+                });
+
+            // delete node after send the mess;
+            var adaRef = admin.database().ref('/notifications/followers/{pushId}');
+            adaRef.remove()
+                .then(function () {
+                    console.log("Remove succeeded.")
+                })
+                .catch(function (error) {
+                    console.log("Remove failed: " + error.message)
+                });
+        });
+    });
+
