09e9e0762a8112717b36740f56e11003664167f1
THANH_PC\HaVan <thanhahvan1@gmail.com>
2017-12-15 17:22:22 +0700

    add function of following, new post, requeting list, granted user

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,12 @@ let functions = require('firebase-functions');
 let admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.sendNotification = functions.database.ref('/notifications/messages/{pushId}')
+
+/**
+ * Triggers when a user gets a new message
+ */
+
+exports.sendMessNotification = functions.database.ref('/notifications/messages/{pushId}')
     .onWrite(event => {
         const message = event.data.current.val();
         const senderUid = message.from;
@@ -25,7 +30,7 @@ exports.sendNotification = functions.database.ref('/notifications/messages/{push
 
             const payload = {
                 notification: {
-                    title: sender.displayName,
+                    title: "New message from: " + sender.displayName,
                     body: message.body,
                 }
             };
@@ -72,7 +77,7 @@ exports.sendFollowerNotification = functions.database.ref('/notifications/follow
             const payload = {
                 notification: {
                     title: "You have new follower!",
-                    body: sender.displayName + "is following you",
+                    body: sender.displayName + " is following you",
                 }
             };
 
@@ -93,6 +98,155 @@ exports.sendFollowerNotification = functions.database.ref('/notifications/follow
                 .catch(function (error) {
                     console.log("Remove failed: " + error.message)
                 });
+            adaRef = null;
+            console.log("Just set the node to null")
+        });
+    });
+
+/**
+ * Triggers when a user send request to a post
+ */
+exports.sendPostRequestNotification = functions.database.ref('/notifications/requesting/{pushId}')
+    .onWrite(event => {
+
+        const post = event.data.current.val(); // 
+        const senderUid = post.from;
+        const receiverUid = message.to;
+        const promises = [];
+
+        const getInstanceIdPromise = admin.database().ref(`/users/${receiverUid}/instanceId`).once('value');
+        const getSenderUidPromise = admin.auth().getUser(senderUid);
+
+        return Promise.all([getInstanceIdPromise, getSenderUidPromise]).then(results => {
+
+            const instanceId = results[0].val();
+            const sender = results[1];
+            console.log('notifying ' + receiverUid + ' about ' + message.body + ' from ' + senderUid);
+
+            const payload = {
+                notification: {
+
+                    title: "Your post got new request",
+                    body: message.body,
+                }
+            };
+
+            admin.messaging().sendToDevice(instanceId, payload)
+                .then(function (response) {
+                    console.log("Successfully sent message:", response);
+                })
+                .catch(function (error) {
+                    console.log("Error sending message:", error);
+                });
+
+            // delete node after send the mess;
+            var adaRef = admin.database().ref('/notifications/requesting/{pushId}');
+            adaRef.remove()
+                .then(function () {
+                    console.log("Remove succeeded.")
+                })
+                .catch(function (error) {
+                    console.log("Remove failed: " + error.message)
+                });
+            adaRef = null;
+            console.log("Just set the node to null")
+        });
+    });
+/**
+ * Triggers when user who being followed post new post.
+ */
+exports.setNewPostNotification = functions.database.ref('/notifications/newPost/{pushId}')
+    .onWrite(event => {
+
+        const post = event.data.current.val(); // 
+        const senderUid = post.re;
+        const receiverUid = message.to;
+        const promises = [];
+
+        const getInstanceIdPromise = admin.database().ref(`/users/${receiverUid}/instanceId`).once('value');
+        const getSenderUidPromise = admin.auth().getUser(senderUid);
+
+        return Promise.all([getInstanceIdPromise, getSenderUidPromise]).then(results => {
+
+            const instanceId = results[0].val();
+            const sender = results[1];
+            console.log('notifying ' + receiverUid + ' about ' + message.body + ' from ' + senderUid);
+
+            const payload = {
+                notification: {
+                    title: sender.displayName + " have a new post",
+                    body: "The user that you are following have new post",
+                }
+            };
+
+            admin.messaging().sendToDevice(instanceId, payload)
+                .then(function (response) {
+                    console.log("Successfully sent message:", response);
+                })
+                .catch(function (error) {
+                    console.log("Error sending message:", error);
+                });
+
+            // delete node after send the mess;
+            var adaRef = admin.database().ref('/notifications/newPost/{pushId}');
+            adaRef.remove()
+                .then(function () {
+                    console.log("Remove succeeded.")
+                })
+                .catch(function (error) {
+                    console.log("Remove failed: " + error.message)
+                });
+            adaRef = null;
+            console.log("Just set the node to null")
+        });
+    });
+
+/**
+ * Triggers when user go granted post notification
+ */
+exports.setNewPostNotification = functions.database.ref('/notifications/granted/{pushId}')
+    .onWrite(event => {
+
+        const post = event.data.current.val(); // 
+        const senderUid = post.re;
+        const receiverUid = message.to;
+        const promises = [];
+
+        const getInstanceIdPromise = admin.database().ref(`/users/${receiverUid}/instanceId`).once('value');
+        const getSenderUidPromise = admin.auth().getUser(senderUid);
+
+        return Promise.all([getInstanceIdPromise, getSenderUidPromise]).then(results => {
+
+            const instanceId = results[0].val();
+            const sender = results[1];
+            console.log('notifying ' + receiverUid + ' about ' + message.body + ' from ' + senderUid);
+
+            const payload = {
+                notification: {
+                    title: sender.displayName + " granted you",
+                    body: "The post that you sent request had been granted to you",
+                }
+            };
+
+            admin.messaging().sendToDevice(instanceId, payload)
+                .then(function (response) {
+                    console.log("Successfully sent message:", response);
+                })
+                .catch(function (error) {
+                    console.log("Error sending message:", error);
+                });
+
+            // delete node after send the mess;
+            var adaRef = admin.database().ref('/notifications/granted/{pushId}');
+            adaRef.remove()
+                .then(function () {
+                    console.log("Remove succeeded.")
+                })
+                .catch(function (error) {
+                    console.log("Remove failed: " + error.message)
+                });
+            adaRef = null;
+            console.log("Just set the node to null")
         });
     });
 
